cd76962cf3a627d6bd6fb0ba5181b7580850ed8c
Maximovex <maximove81@gmail.com>
2024-08-28 22:12:51 +0300

    Add files via upload

---
+++ module7_4.py
@@ -0,0 +1,16 @@
+team1_num=5
+team2_num=6
+print("В команде Мастера кода участников: %a! "%team1_num)
+print("Итого сегодня в командах участников: %s и %s!"% (team1_num, team2_num))
+score_2=35
+print("Команда Волшебники данных решила задач: {}!".format(score_2))
+team2_time=2043.8
+print("Волшебники данных решили задачи за {}с!".format(team2_time))
+
+score_1=40
+print(f'Команды решили {score_1} и {score_2} задач.')
+challenge_result='Мастера кода'
+print(f'Результат битвы: победа команды {challenge_result}!')
+tasks_total=score_2+score_1
+time_avg=56.4
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
